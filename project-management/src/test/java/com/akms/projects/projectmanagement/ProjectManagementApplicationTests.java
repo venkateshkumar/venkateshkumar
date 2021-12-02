3ab6a26d6205ad872dfed2d42461f598d614a8cd
Venkateshkumar S <venkateshkumarsr@gmail.com>
2021-12-03 00:24:08 +0400

    Add files via upload
    Project management base files uploaded

---
+++ project-management/src/test/java/com/akms/projects/projectmanagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akms.projects.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
